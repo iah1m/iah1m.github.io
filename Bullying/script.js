f3021e19f4e612e119f81441d884c64fc4f4140e
thatANSR <ansremail@gmail.com>
2021-05-13 14:19:20 +0300

    First iteration of Bullying game

--- Bullying/script.js
+++ Bullying/script.js
@@ -1,5 +1,48 @@
-document.getElementById("nextButton").addEventListener("click",nextCard);
+window.onload=function setButtons()
+{
 
-function nextCard() {
-  alert("Next");  // The function returns the product of p1 and p2
+  var cardTexts=[];
+
+  var workArray=[];
+
+
+  cardTexts.push('Mar');
+  cardTexts.push('Banana');
+  cardTexts.push('Para');
+
+  document.getElementById("nextButton").addEventListener("click",nextCard);
+  document.getElementById("resetButton").addEventListener("click",resetCards);
+
+  function setWorkArray()
+  {
+    workArray=[];
+    cardTexts.forEach(createWorkArray);
+  }
+  function createWorkArray(value)
+  {
+    workArray.push(value);
+  }
+  setWorkArray();
+
+  function nextCard() 
+  {
+    var randomItem = workArray[Math.floor(Math.random()*workArray.length)];
+    var removeIndex=workArray.indexOf(randomItem);
+    if(removeIndex==-1)
+    document.getElementById("textWrapper").innerHTML="Nu mai exista cartonase."; 
+    else
+    {
+      workArray.splice(removeIndex,1);
+      document.getElementById("textWrapper").innerHTML=randomItem; 
+    }
+    
+  }
+
+  function resetCards() 
+  {
+    setWorkArray();
+  }
+
+
 }
+
